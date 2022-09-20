a240361ad0ce240929eda87123852c497b276a61
Geoff Corey <geoff@geoffcorey.com>
2022-09-20 12:23:32 -0400

    use neovim instead of vtr for vim-test

--- lua/user/vim-test.lua
+++ lua/user/vim-test.lua
@@ -3,7 +3,7 @@ local opts = { silent = true }
 vim.g.tslime_always_current_session = 1
 vim.g.tslime_always_current_window = 1
 
-vim.api.nvim_set_var("test#strategy", "vtr")
+vim.api.nvim_set_var("test#strategy", "neovim")
 vim.api.nvim_set_var("test#preserve_screen", 1)
 vim.api.nvim_set_var("test#filename_modifier", ":~")
 -- vim.api.nvim_set_var("test#runner_commands", ["mocha"])
